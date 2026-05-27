c13fa86293606afef383de94e08b6c36c28ca343
unknown <mattjtollie@gmail.com>
2026-05-27 00:13:49 -0600

    fix: led test script - adding correct dependencies and memoryx file

--- firmware/src/bin/led_test.rs
+++ firmware/src/bin/led_test.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(impl_trait_in_assoc_type)]
 
 use defmt::*;
 use defmt_rtt as _;
@@ -13,7 +12,27 @@ use embassy_time::Timer;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    let p = embassy_stm32::init(Config::default());
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: embassy_stm32::time::Hertz(8_000_000),
+            mode: HseMode::Bypass,
+        });
+        config.rcc.pll_src = PllSource::Hse;
+        config.rcc.pll = Some(Pll {
+            prediv: PllPreDiv::Div4,
+            mul: PllMul::Mul100,
+            divp: Some(PllPDiv::Div2),
+            divq: None,
+            divr: None,
+        });
+        config.rcc.sys = Sysclk::Pll1P;
+        config.rcc.ahb_pre = AHBPrescaler::Div1;
+        config.rcc.apb1_pre = APBPrescaler::Div2;
+        config.rcc.apb2_pre = APBPrescaler::Div1;
+    }
+    let p = embassy_stm32::init(config);
 
     let mut green = Output::new(p.PB3, Level::Low, Speed::Low);
     let mut red = Output::new(p.PB5, Level::Low, Speed::Low);
@@ -23,22 +42,22 @@ async fn main(_spawner: Spawner) -> ! {
     loop {
         info!("green on");
         green.set_high();
-        Timer::after_millis(500).await;
+        Timer::after_millis(100).await;
         green.set_low();
-        Timer::after_millis(200).await;
+        Timer::after_millis(50).await;
 
         info!("red on");
         red.set_high();
-        Timer::after_millis(500).await;
+        Timer::after_millis(100).await;
         red.set_low();
-        Timer::after_millis(200).await;
+        Timer::after_millis(50).await;
 
         info!("both on");
         green.set_high();
         red.set_high();
-        Timer::after_millis(500).await;
+        Timer::after_millis(100).await;
         green.set_low();
         red.set_low();
-        Timer::after_millis(500).await;
+        Timer::after_millis(50).await;
     }
 }
